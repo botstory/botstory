e5570d9a1675a49c7249ce5e5f1f54b7800214d3
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-10-05 02:25:07 +0200

    open/close mongo session  between classes

--- botstory/integrations/mongodb/db_test.py
+++ botstory/integrations/mongodb/db_test.py
@@ -4,33 +4,50 @@ import pytest
 from . import db
 from ... import utils
 
-
 logger = logging.getLogger(__name__)
 
+
+@pytest.fixture
 @pytest.mark.asyncio
-async def test_store_restore_user(event_loop):
-    db_interface = db.MongodbInterface(uri='mongo', db_name='test')
-    await db_interface.connect(loop=event_loop)
-    user = utils.build_fake_user()
+def open_db(event_loop):
+    class AsyncDBConnection:
+        def __init__(self):
+            self.db_interface = db.MongodbInterface(uri='mongo', db_name='test')
 
-    user_id = await db_interface.set_user(user._response)
-    restored_user = await db_interface.get_user(user_id)
+        async def __aenter__(self):
+            await self.db_interface.connect(loop=event_loop)
+            return self.db_interface
 
-    assert user.items() == restored_user.items()
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            self.db_interface = None
+
+    return AsyncDBConnection
+
 
 @pytest.mark.asyncio
-async def test_store_restore_session(event_loop):
-    db_interface = db.MongodbInterface(uri='mongo', db_name='test')
-    await db_interface.connect(loop=event_loop)
-    session = utils.build_fake_session()
-    user_id = '123456789'
+async def test_store_restore_user(open_db):
+    async with open_db() as db_interface:
+        user = utils.build_fake_user()
 
-    session_id = await db_interface.set_session(user_id, session._response)
-    logger.debug('session_id')
-    logger.debug(session_id)
-    restored_session = await db_interface.get_session(session_id=session_id)
-    logger.debug('restored_session')
-    logger.debug(restored_session)
-    restored_session = await db_interface.get_session(user_id=user_id)
+        user_id = await db_interface.set_user(user._response)
+        restored_user = await db_interface.get_user(user_id)
 
-    assert list(session.items()) == [(key, value) for (key, value) in restored_session.items() if key not in ['_id']]
+        assert user.items() == restored_user.items()
+
+
+@pytest.mark.asyncio
+async def test_store_restore_session(open_db):
+    async with open_db() as db_interface:
+        session = utils.build_fake_session()
+        user_id = '123456789'
+
+        session_id = await db_interface.set_session(user_id, session._response)
+        logger.debug('session_id')
+        logger.debug(session_id)
+        restored_session = await db_interface.get_session(session_id=session_id)
+        logger.debug('restored_session')
+        logger.debug(restored_session)
+        restored_session = await db_interface.get_session(user_id=user_id)
+
+        assert list(session.items()) == [(key, value) for (key, value) in restored_session.items() if
+                                         key not in ['_id']]
