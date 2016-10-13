caeaf372a848c44f500144675248644ae6992a77
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-10-14 00:49:24 +0200

    bump to 0.0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if os.path.exists("README.md"):
 setup(
     name='botstory',
     packages=find_packages(),
-    version='0.0.7',
+    version='0.0.8',
     description='Async framework for bots',
     license='MIT',
     long_description=longDesc,
