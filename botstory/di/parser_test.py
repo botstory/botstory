565aab9d9cf4fb1fae101c1bb2a93ae730e687f2
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-11-22 10:14:51 +0100

    empty array on non class name value

--- botstory/di/parser_test.py
+++ botstory/di/parser_test.py
@@ -7,3 +7,7 @@ def test_camelcase_to_underscore():
 
 def test_remove_leading_underscore():
     assert camel_case_to_underscore('_ClassName')[0] == 'class_name'
+
+
+def test_should_return_empty_array_if_no_any_class_name_here():
+    assert camel_case_to_underscore('_qwerty') == []
