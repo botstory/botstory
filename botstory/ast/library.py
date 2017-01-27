bc98b6e0e6a6c8269f17d74699fbbe953558a7b7
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-01-27 11:42:30 +0100

    simplify

--- botstory/ast/library.py
+++ botstory/ast/library.py
@@ -37,7 +37,7 @@ class StoriesLibrary:
                            if story.extensions['validator'].validate(message)]
         return matched_stories[0] if len(matched_stories) > 0 else None
 
-    def get_story_by_topic(self, topic, stack=[]):
+    def get_story_by_topic(self, topic, stack=None):
         """
         get story and take about context stack
         :param topic:
@@ -49,22 +49,22 @@ class StoriesLibrary:
         if len(options) > 0:
             return options[0]
 
-        if len(options) == 0 and stack and len(stack) > 0:
-            parent = self.get_story_by_topic(stack[-1]['topic'], stack[:-2])
-            if not parent:
-                return None
-            inner_stories = [
-                story.children for story in parent.story_line if isinstance(story, parser.StoryPartFork)
-            ]
+        if not stack or len(stack) == 0:
+            return None
 
-            inner_stories = [item for sublist in inner_stories for item in sublist]
+        parent = self.get_story_by_topic(stack[-1]['topic'], stack[:-2])
+        if not parent:
+            return None
+        inner_stories = [
+            story.children for story in parent.story_line if isinstance(story, parser.StoryPartFork)
+        ]
 
-            child_options = [story for story in inner_stories if story.topic == topic]
-            if len(child_options) == 0:
-                return None
-            elif len(child_options) == 1:
-                return child_options[0]
-            else:
-                raise Error('We have few options with the same name {}'.format(topic))
+        inner_stories = [item for sublist in inner_stories for item in sublist]
 
-        return None
+        child_options = [story for story in inner_stories if story.topic == topic]
+        if len(child_options) == 0:
+            return None
+        elif len(child_options) == 1:
+            return child_options[0]
+        else:
+            raise Error('We have few options with the same name {}'.format(topic))
