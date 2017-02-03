dada4e023de4a6ef5bf60d90f4e4d80564cbaf5c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-02-04 00:14:51 +0100

    merge match_message and _match_message

--- botstory/ast/processor.py
+++ botstory/ast/processor.py
@@ -44,105 +44,95 @@ class StoryProcessor:
             data=message['data'],
         )
 
+        session = message['session']
         stack = message['session']['stack']
 
         waiting_for = None
 
-        # TODO: it seems that it could be merge with loop in _match_message
-        # because they do similar things - bubble up the stack
         while not waiting_for or isinstance(waiting_for, callable.EndOfStory):
-            # but it seems that we have hierarchy of callable
-            # stories so we should drop current stack element
-            # because it is over and return to the previous story
-            waiting_for = await self._match_message(message)
+            compiled_story = None
+            validation_result = None
+            stack_tail = None
+
+            if len(session['stack']) > 0:
+                logger.debug('  check stack')
+                logger.debug('  session = {}'.format(session))
+                logger.debug('    session.stack = {}'.format(session['stack']))
+
+                # looking for first valid matcher
+                while True:
+                    if len(session['stack']) == 0:
+                        # we have reach the bottom of stack
+                        logger.debug('  we have reach the bottom of stack '
+                                     'so no once has receive this message')
+                        return None
+                    stack_tail = session['stack'].pop()
+                    logger.debug('stack_tail {}'.format(stack_tail))
+                    if not stack_tail['data']:
+                        # TODO: we shouldn't get such case
+                        # because it shows that we have stack that can't catch incoming message
+                        # (?) maybe possible case when it is in a middle of hierarchy
+                        # and comes because the top one didn't match message
+
+                        # one case - dangling of last part of story
+                        # we don't clear stack after returning from story
+                        break
+                    validator = matchers.deserialize(stack_tail['data'])
+                    logger.debug('validator {}'.format(validator))
+
+                    if getattr(validator, 'new_scope', False):
+                        # TODO:
+                        # we are start new story line here
+                        # so we don't need to check whether we still have tail of story
+                        break
+
+                    logger.debug("stack_tail['topic'] {}".format(stack_tail['topic']))
+                    logger.debug("session['stack'] {}".format(session['stack']))
+                    logger.debug("self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack'])")
+                    logger.debug(self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack']))
+                    if stack_tail['step'] < len(
+                            self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack']).story_line
+                    ):
+                        # if we haven't reach last step in list of story so we can parse result
+                        break
+
+                logger.debug('    after check session.stack = {}'.format(session['stack']))
+                logger.debug('      stack_tail = {}'.format(stack_tail))
+                if stack_tail and stack_tail['data']:
+                    logger.debug('  got it!')
+                    validator = matchers.deserialize(stack_tail['data'])
+                    validation_result = validator.validate(message)
+                    logger.debug('      validation_result {}'.format(validation_result))
+                    if not not validation_result:
+                        # it seems we find stack item that matches our message
+                        compiled_story = self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack'])
+
+            if not compiled_story:
+                compiled_story = self.library.get_global_story(message)
+                logger.debug('get_global_story {}'.format(compiled_story))
+                if not compiled_story:
+                    # there is no stories for such message
+                    return True
+                stack_tail = stack_utils.build_empty_stack_item(compiled_story.topic)
+
+            session['stack'].append(stack_tail)
+
+            received_data = await self.process_next_part_of_story({
+                'step': session['stack'][-1]['step'],
+                'story': compiled_story,
+                'stack': message['session']['stack'],
+            }, validation_result)
+
+            waiting_for = await self.process_story(
+                message=message,
+                compiled_story=received_data['story'],
+                session=message['session'],
+            )
 
             if len(stack) == 0:
                 break
 
         return waiting_for
-
-    async def _match_message(self, message):
-        session = message['session']
-        logger.debug('_match_message')
-
-        compiled_story = None
-        validation_result = None
-        stack_tail = None
-
-        if len(session['stack']) > 0:
-            logger.debug('  check stack')
-            logger.debug('  session = {}'.format(session))
-            logger.debug('    session.stack = {}'.format(session['stack']))
-
-            # looking for first valid matcher
-            while True:
-                if len(session['stack']) == 0:
-                    # we have reach the bottom of stack
-                    logger.debug('  we have reach the bottom of stack '
-                                 'so no once has receive this message')
-                    return None
-                stack_tail = session['stack'].pop()
-                logger.debug('stack_tail {}'.format(stack_tail))
-                if not stack_tail['data']:
-                    # TODO: we shouldn't get such case
-                    # because it shows that we have stack that can't catch incoming message
-                    # (?) maybe possible case when it is in a middle of hierarchy
-                    # and comes because the top one didn't match message
-
-                    # one case - dangling of last part of story
-                    # we don't clear stack after returning from story
-                    break
-                validator = matchers.deserialize(stack_tail['data'])
-                logger.debug('validator {}'.format(validator))
-
-                if getattr(validator, 'new_scope', False):
-                    # TODO:
-                    # we are start new story line here
-                    # so we don't need to check whether we still have tail of story
-                    break
-
-                logger.debug("stack_tail['topic'] {}".format(stack_tail['topic']))
-                logger.debug("session['stack'] {}".format(session['stack']))
-                logger.debug("self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack'])")
-                logger.debug(self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack']))
-                if stack_tail['step'] < len(
-                        self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack']).story_line
-                ):
-                    # if we haven't reach last step in list of story so we can parse result
-                    break
-
-            logger.debug('    after check session.stack = {}'.format(session['stack']))
-            logger.debug('      stack_tail = {}'.format(stack_tail))
-            if stack_tail and stack_tail['data']:
-                logger.debug('  got it!')
-                validator = matchers.deserialize(stack_tail['data'])
-                validation_result = validator.validate(message)
-                logger.debug('      validation_result {}'.format(validation_result))
-                if not not validation_result:
-                    # it seems we find stack item that matches our message
-                    compiled_story = self.library.get_story_by_topic(stack_tail['topic'], stack=session['stack'])
-
-        if not compiled_story:
-            compiled_story = self.library.get_global_story(message)
-            logger.debug('get_global_story {}'.format(compiled_story))
-            if not compiled_story:
-                # there is no stories for such message
-                return True
-            stack_tail = stack_utils.build_empty_stack_item(compiled_story.topic)
-
-        session['stack'].append(stack_tail)
-
-        received_data = await self.process_next_part_of_story({
-            'step': session['stack'][-1]['step'],
-            'story': compiled_story,
-            'stack': message['session']['stack'],
-        }, validation_result)
-
-        return await self.process_story(
-            message=message,
-            compiled_story=received_data['story'],
-            session=message['session'],
-        )
 
     async def process_next_part_of_story(self, received_data, validation_result):
         logger.debug('')
