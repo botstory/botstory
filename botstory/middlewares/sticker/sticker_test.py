8b24be5ade7a0c28c379f89b4465a8fc977ebaee
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-03-15 11:38:41 +0100

    could match different size of likes

--- botstory/middlewares/sticker/sticker_test.py
+++ botstory/middlewares/sticker/sticker_test.py
@@ -58,3 +58,47 @@ async def test_match_fblike_sticker():
         })
 
         assert trigger_like.is_triggered
+
+
+@pytest.mark.parametrize(('match_size', 'message_size'),
+                         [(sticker.SMALL_LIKE, sticker.SMALL_LIKE),
+                          (sticker.MEDIUM_LIKE, sticker.MEDIUM_LIKE),
+                          (sticker.BIG_LIKE, sticker.BIG_LIKE),
+                          ])
+@pytest.mark.asyncio
+async def test_match_different_size_of_fblike(match_size, message_size):
+    trigger_any = utils.SimpleTrigger()
+    trigger_likes = {size: utils.SimpleTrigger() for size in list(sticker.LIKE_STICKERS)}
+
+    with answer.Talk() as talk:
+        story = talk.story
+
+        @story.on(sticker.Like(sticker.SMALL_LIKE))
+        def small_like_sticker_story():
+            @story.part()
+            def then(ctx):
+                trigger_likes[sticker.SMALL_LIKE].passed()
+
+        @story.on(sticker.Like(sticker.MEDIUM_LIKE))
+        def medium_like_sticker_story():
+            @story.part()
+            def then(ctx):
+                trigger_likes[sticker.MEDIUM_LIKE].passed()
+
+        @story.on(sticker.Like(sticker.BIG_LIKE))
+        def big_like_sticker_story():
+            @story.part()
+            def then(ctx):
+                trigger_likes[sticker.BIG_LIKE].passed()
+
+        @story.on(sticker.Any())
+        def any_like_sticker_story():
+            @story.part()
+            def then(ctx):
+                trigger_any.passed()
+
+        await talk.ask({
+            'sticker_id': message_size,
+        })
+
+        assert trigger_likes[match_size].is_triggered
